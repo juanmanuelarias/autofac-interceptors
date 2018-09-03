ea8f35b1fdb48b2ff03159589d3a5996878dafaa
Juan Arias <juan.arias@xero.com>
2018-09-03 20:20:24 +1200

    Add project files.

---
+++ InterceptorsExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterceptorsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
